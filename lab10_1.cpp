9c1c817cfd892e986c9367dbfefee33e87e5f5d9
Your Name <you@example.com>
2022-12-27 14:50:21 +0700

    completed lab10_1

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,14 @@
 using namespace std;
 
 int main(){	
+	double loan,interest,payment;
+	int year=1;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
 	cout << "Enter amount you can pay per year: ";
+	cin >> payment;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -20,14 +25,21 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+
+	while (loan>0){
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << year; 
+		year++;
+		cout << setw(13) << left << loan;
+		cout << setw(13) << left << interest*loan/100;
+		loan+=interest*loan/100;
+		cout << setw(13) << left << loan;
+		if (loan<payment) payment=loan;
+		cout << setw(13) << left << payment;
+		loan-=payment;
+		cout << setw(13) << left << loan;
+		cout << "\n";	
+	}
 	
 	return 0;
 }
